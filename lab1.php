c704e2b78a16504fcf406444010216a8d7440f37
Ilya Butorin <ilya.butorin2004@gmail.com>
2023-03-07 14:12:20 +0300

    lab4

--- lab1.php
+++ lab1.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Лабораторная работа 1</title>
-  <link rel="stylesheet" href="../css/lab1.css">
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="css/lab1.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
--- lab4.php
+++ lab4.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Лабораторная работа 1</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/lab4.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/lab4.css">
 </head>
 
 <body>
@@ -18,24 +18,90 @@
         <div class="container">
             <h2 class="container__heading">Введите уравнение типа a + b = c</h2>
             <form action="" method="POST">
-                <input value='5 + x = 10' type="text" name="eq" class="input" placeholder="a + b = c">
+                <input type="text" name="equation" class="input" placeholder="a + b = c">
                 <button type="submit" class="get-answer">Посчитать</button>
             </form>
 
 
-            <textarea >
+            <div>
+                <p class="answer">
                 <?php
-                if (isset($_POST)){
-                    $str = $_POST['eq'];
+                function getxpos($str)
+                {
+                    return strpos($str, 'x');
+                }
+
+                if (!empty($_POST)) {
+                    $str = $_POST['equation'];
                     $str = str_replace(' ', '', $str);
-                    function getxpos($str)
-                    {
-                        return strpos($str, 'x');
-                    };
-                    print_r(getxpos($str));
-                };
+                    $str = strtolower($str);
+                    $arr_equation = explode('=', $str);
+                    $result = $arr_equation[1];
+                    if (strpos($arr_equation[0], '+') !== false) {
+                        $operand = '+';
+                    }
+                    if (strpos($arr_equation[0], '-') !== false) {
+                        $operand = '-';
+                    }
+                    if (strpos($arr_equation[0], '*') !== false) {
+                        $operand = '*';
+                    }
+                    if (strpos($arr_equation[0], '/') !== false) {
+                        $operand = '/';
+                    }
+                    // equatation split
+                    if ($operand == '/') {
+                        $equatation = explode('/', $arr_equation[0]);
+                    }
+                    if ($operand == '*') {
+                        $equatation = explode('*', $arr_equation[0]);
+                    }
+                    if ($operand == '-') {
+                        $equatation = explode('-', $arr_equation[0]);
+                    }
+                    if ($operand == '+') {
+                        $equatation = explode('+', $arr_equation[0]);
+                    }
+                    // counting
+                    if ($operand == '+') {
+                        if ($equatation[0] == 'x') {
+                            $x = (int)$result - (int)$equatation[1];
+                        } elseif ($equatation[1] == 'x') {
+                            $x = (int)$result - (int)$equatation[0];
+                        } elseif($result == 'x') {
+                            $x = (int)$equatation[0] + (int)$equatation[1];
+                        }
+                    } elseif ($operand == '-') {
+                        if ($equatation[0] == 'x') {
+                            $x = (int)$result + (int)$equatation[1];
+                        } elseif ($equatation[1] == 'x') {
+                            $x = (int)$equatation[0] - (int)$result;
+                        } elseif($result == 'x') {
+                            $x = (int)$equatation[0] - (int)$equatation[1];
+                        }
+                    } elseif ($operand == '*') {
+                        if ($equatation[0] == 'x') {
+                            $x = (int)$result / (int)$equatation[1];
+                        } elseif ($equatation[1] == 'x') {
+                            $x = (int)$result / (int)$equatation[0];
+                        } elseif($result == 'x') {
+                            $x = (int)$equatation[0] * (int)$equatation[1];
+                        }
+                    } elseif ($operand =='/') {
+                        if ($equatation[0] == 'x') {
+                            $x = (int)$result * (int)$equatation[1];
+                        } elseif ($equatation[1] == 'x') {
+                            $x = (int)$equatation[0] / (int)$result;
+                        } elseif($result == 'x') {
+                            $x = (int)$equatation[0] / (int)$equatation[1];
+                        }
+                    }
+               
+                    echo $x;
+                }
                 ?>
-            </textarea>
+                </p>
+            </div>
         </div>
     </section>
     <?php require "blocks/footer.php" ?>
